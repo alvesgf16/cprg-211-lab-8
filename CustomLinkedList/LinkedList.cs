829118ea992ebac7033110b2c8a53dd76ef1a36a
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-03-21 21:55:32 -0600

    feat: add node to the end of the list

--- CustomLinkedList/LinkedList.cs
+++ CustomLinkedList/LinkedList.cs
@@ -12,7 +12,7 @@ public class LinkedList()
             int counter = 0;
 
             // traverse through the linked list
-            while (temp != null)
+            while (temp is not null)
             {
                 counter += 1;
                 temp = temp.Next;
@@ -30,5 +30,27 @@ public class LinkedList()
         };
 
         Head = newNode;
+    }
+
+    public void AddLast(string data)
+    {
+        Node newNode = new(data);
+
+        if (Head is null)
+        {
+            Head = newNode;
+            return;
+        }
+
+        Node temp = Head;
+
+        // traverse to the last node
+        while (temp.Next is not null)
+        {
+            temp = temp.Next;
+        }
+
+        // point the next of the last element to the new node
+        temp.Next = newNode;
     }
 }
--- LinkedListTests/UnitTest1.cs
+++ LinkedListTests/UnitTest1.cs
@@ -32,4 +32,55 @@ public class Tests
         Assert.That(list.Count, Is.EqualTo(2));
         Assert.That(list.Head!.Data, Is.EqualTo("Joe Blow"));
     }
+
+    [Test]
+    public void AddLast_EmptyList_AddsNodeToEnd()
+    {
+        // Arrange
+        LinkedList list = new LinkedList();
+
+        // Act
+        list.AddLast("Joe Blow");
+
+        // Assert
+        Assert.That(list.Count, Is.EqualTo(1));
+        Assert.That(list.Head!.Data, Is.EqualTo("Joe Blow"));
+        Assert.IsNull(list.Head.Next);
+    }
+
+    [Test]
+    public void AddLast_NonEmptyList_AddsNodeToEnd()
+    {
+        // Arrange
+        LinkedList list = new LinkedList();
+        list.AddLast("Joe Schmoe");
+
+        // Act
+        list.AddLast("Joe Blow");
+
+        // Assert
+        Assert.That(list.Count, Is.EqualTo(2));
+        Assert.That(list.Head!.Data, Is.EqualTo("Joe Schmoe"));
+        Assert.That(list.Head.Next!.Data, Is.EqualTo("Joe Blow"));
+        Assert.IsNull(list.Head.Next.Next);
+    }
+
+    [Test]
+    public void AddLast_MultipleAdds_AddsNodesInCorrectOrder()
+    {
+        // Arrange
+        LinkedList list = new LinkedList();
+        list.AddLast("Joe Schmoe");
+        list.AddLast("Joe Blow");
+
+        // Act
+        list.AddLast("John Smith");
+
+        // Assert
+        Assert.That(list.Count, Is.EqualTo(3));
+        Assert.That(list.Head!.Data, Is.EqualTo("Joe Schmoe"));
+        Assert.That(list.Head.Next!.Data, Is.EqualTo("Joe Blow"));
+        Assert.That(list.Head.Next.Next!.Data, Is.EqualTo("John Smith"));
+        Assert.IsNull(list.Head.Next.Next.Next);
+    }
 }
